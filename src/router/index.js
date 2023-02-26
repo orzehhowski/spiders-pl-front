5a9802a1620ca60f169a99762353d2a9868e7289
orzehhowski <filmyminecraftnayt@gmail.com>
2023-02-26 13:12:01 +0100

    404 page added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +20,7 @@ const router = createRouter({
       name: "family",
       component: () => import("../views/FamilyView.vue"),
     },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundView },
   ],
 });
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="container">
+    <h1 class="mt-3">No nieee, znowu 404 :c</h1>
+    <hr />
+    <h2 class="mt-2">Taka strona nie istnieje!</h2>
+  </main>
+</template>
