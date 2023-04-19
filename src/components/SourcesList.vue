1655f40570164919e735d88fe9682a5f4cd38377
orzehhowski <filmyminecraftnayt@gmail.com>
2023-04-19 18:58:12 +0200

    file name change

---
+++ src/components/SourcesList.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    sources: Array,
+  },
+};
+</script>
+
+<template>
+  <div v-if="sources.length > 0">
+    <h5>Źródła zewnętrzne</h5>
+    <hr class="small" />
+    <div class="mb-4">
+      <p v-for="(source, index) in sources" :key="index" class="sources">
+        <a :href="source" target="_blank" rel="noopener">{{ source }}</a>
+      </p>
+    </div>
+  </div>
+</template>
